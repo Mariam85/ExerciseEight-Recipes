90cf70945c7ad078b1bc9440c1925b06b64d899f
Mariam85 <mariam.ali00@eng-st.cu.edu.eg>
2022-09-27 06:41:08 +0200

    Added some of the seed files.

---
+++ RecipeMigrations/Seeds/_1001_RecipeTableSeed.cs
@@ -0,0 +1,77 @@
+﻿using FluentMigrator;
+using RecipeMigrations.Migrations;
+
+namespace RecipeMigrations.Seeds
+{
+    public record Recipe
+    {
+        public string Title { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+    [Migration(1001)]
+    public class _1001_RecipeTableSeed : Migration
+    {
+        public static List<Recipe> recipes = new()
+        {
+            new Recipe
+            {
+                Title = "Miso Mushroom Pasta",
+                IsActive = true
+            },
+            new Recipe
+            {
+                Title = "Shrimp Tacos",
+                IsActive = true
+            },
+            new Recipe
+            {
+                Title = "Beef Bulgogi",
+                IsActive = true
+            },
+            new Recipe
+            {
+                Title = "Pasta Carbonara",
+                IsActive = true
+            },
+            new Recipe
+            {
+                Title = "Grilled Pork Chops",
+                IsActive = true
+            },
+            new Recipe
+            {
+                Title = "Thai fried rice",
+                IsActive = true
+            },
+            new Recipe
+            {
+                Title = "Vegan icecream",
+                IsActive = true
+            },
+            new Recipe
+            {
+                Title = "Greek salad",
+                IsActive = true
+            }
+        };
+
+        public override void Up()
+        {
+            foreach (var recipe in recipes)
+            {
+                Insert.IntoTable("recipe")
+                    .Row(new
+                    {
+                        is_active = recipe.IsActive,
+                        title = recipe.Title
+                    });
+            }
+        }
+
+        public override void Down()
+        {
+            //empty, not using
+        }
+    }
+}
